1223716c1fdd34a4b9c22ceeaba865a8e1c57eec
volha <v.khviadchenia@gmail.com>
2021-01-18 00:31:44 +0100

    feat: add Dropdown in Header

--- clone-wars/src/index.js
+++ clone-wars/src/index.js
@@ -1,2 +1,4 @@
 // eslint-disable-next-line no-unused-vars
-import style from './style/style.css';
+import style from "./style/style.css";
+
+// const mainContent = document.querySelector("#mainСontent");
