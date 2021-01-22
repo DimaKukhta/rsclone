4ecc8bd4040f92670b67a2bf15b36aa1d261c8b1
volha <v.khviadchenia@gmail.com>
2021-01-22 05:33:31 +0100

    feat: add data for expense categories

--- clone-wars/src/index.js
+++ clone-wars/src/index.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line no-unused-vars
-import style from "./style/style.css";
+import style from './style/style.css';
+import { expenseCategoriesEN } from './js/data/DataExpenseCategories';
 
-// const mainContent = document.querySelector("#mainСontent");
+const mainContent = document.querySelector('#main-content');
+const elem = expenseCategoriesEN[0].icon;
+mainContent.append(elem);
