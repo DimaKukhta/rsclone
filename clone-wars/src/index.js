36c186085164cf4e514455738b1e5744b13e3172
volha <v.khviadchenia@gmail.com>
2021-01-22 16:44:51 +0100

    feat: update previous function

--- clone-wars/src/index.js
+++ clone-wars/src/index.js
@@ -10,6 +10,7 @@ const btnAddIncome = document.querySelector('#btn-add-income');
 btnAddExpense.addEventListener('click', addExpense);
 btnAddIncome.addEventListener('click', addIncome);
 
+// начало огромной функции, которая будет разбита на части и перенесена в отдельные файлы :)
 function addExpense() {
   mainContent.innerHTML = '';
 
@@ -17,10 +18,10 @@ function addExpense() {
   const container = document.createElement('div');
   container.classList.add('d-flex', 'flex-column', 'text-center', 'mt-5');
 
-  const label = document.createElement('label');
-  label.classList.add('mb-1', 'text-success', 'fw-bold');
-  label.for = 'category-select';
-  label.textContent = 'Select category';
+  const labelCategory = document.createElement('labelCategory');
+  labelCategory.classList.add('mb-1', 'text-success', 'fw-bold');
+  labelCategory.for = 'category-select';
+  labelCategory.textContent = 'Select category';
 
   const select = document.createElement('select');
   select.classList.add('form-select', 'col-5', 'mb-4');
@@ -31,11 +32,30 @@ function addExpense() {
   firstOption.value = 'none';
   firstOption.textContent = '-- choose one --';
 
-  const listOptions = EnExpenseCategories.map()
+  // const listOptions = EnExpenseCategories.map() тут допишу создание опций из файлы с данными категорий
+  const labelAmount = document.createElement('label');
+  labelAmount.classList.add('mb-1', 'text-success', 'fw-bold');
+  labelAmount.for = 'add-amount';
+  labelAmount.textContent = 'Add amount';
 
+  const input = document.createElement('input');
+  input.classList.add('form-control', 'input-expense', 'mb-5');
+  input.id = 'add-amount';
+  input.placeholder = '-- enter the amount --';
+  input.disabled = true;
+
+  const btn = document.createElement('button');
+  btn.classList.add('btn', 'btn-success', 'mx-auto', 'w-100');
+  btn.id = 'save';
+  btn.disabled = true;
+  btn.textContent = 'Save expense';
+
+  const audio = document.createElement('audio');
+  audio.id = 'audio';
+  audio.src = './assets/audio/expense.mp3';
 
   select.append(firstOption);
-  container.append(label, select);
+  container.append(labelCategory, select, labelAmount, input, btn, audio);
   fragment.append(container);
 
   mainContent.append(fragment);
@@ -44,3 +64,5 @@ function addExpense() {
 function addIncome() {
   mainContent.innerHTML = '';
 }
+
+// конец огромной функции, которая будет разбита на части и перенесена в отдельные файлы :)
