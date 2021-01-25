ad9f1d31ec2b545c8edafd4b7ac9c104ed8cf76d
Dima Kukhta <dima.kukhta.2001@mail.ru>
2021-01-25 20:06:23 +0200

    feat: add function

---
+++ clone-wars/src/js/authorization/authorization.js
@@ -0,0 +1,19 @@
+export default function getAuthorizationWindow() {
+    const htmlCode = `<div class="container-form">
+    <form class="authorization-form">
+      <div class="mb-3">
+        <label for="InputLogin" class="form-label">Логин</label>
+        <input type="login" class="form-control" id="InputLogin" aria-describedby="loginHelp">
+        <div id="loginHelp" class="form-text">Введите ваши данные</div>
+      </div>
+      <div class="mb-3">
+        <label for="InputPassword" class="form-label">Пароль</label>
+        <input type="password" class="form-control" id="InputPassword">
+      </div>
+      <div class="mb-3 button-form">
+        <button class="btn btn-primary">Войти в аккаунт</button>
+        <button class="btn btn-primary">Новый аккаунт</button>
+      </div>
+    </form>
+  </div>`;
+}
