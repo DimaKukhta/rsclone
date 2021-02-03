4541b1904cbc60d7b9b91959692ba383e99da5c9
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-02-03 12:03:50 +0300

    Update drawHotKeys.js

--- clone-wars/src/js/drawHotKeys/drawHotKeys.js
+++ clone-wars/src/js/drawHotKeys/drawHotKeys.js
@@ -1,24 +1,26 @@
+import { switchLang } from '../data/baselayoutLang';
+
 function drawTable() {
   const mainContent = document.querySelector('#main-content');
   location.hash = 'hot-keys';
   mainContent.innerHTML = '';
 
-  mainContent.insertAdjacentHTML('beforeend', `<div class="hotkeys-table">
-  <h3 class="text-success">In the project you can use the following hotkeys</h3>
+  const html = `<div class="hotkeys-table">
+  <h3 class="text-success">${switchLang().firstTableTitle}</h3>
   <table>
     <tr>
-      <th> BUTTONS</th>
-      <th>"Operations"</th>
-      <th> "Reports"</th>
-      <th> "Add operating"</th>
-      <th> "Hotkeys"</th>
-      <th> "Settings"</th>
-      <th> "Previous period"</th>
-      <th> "Next period"</th>
-      <th> "Select period"</th>
+      <th>${switchLang().buttons.toUpperCase()}</th>
+      <th>${switchLang().operations}</th>
+      <th> ${switchLang().reports}</th>
+      <th> ${switchLang().add}</th>
+      <th> ${switchLang().hotKeys}</th>
+      <th> ${switchLang().settings}</th>
+      <th> ${switchLang().prevPeriod}</th>
+      <th> ${switchLang().nextPeriod}</th>
+      <th> ${switchLang().selectPeriod}</th>
     </tr>
     <tr>
-      <td class="fw-bold">HOTKEYS</td>
+      <td class="fw-bold">${switchLang().hotkeys.toUpperCase()}</td>
       <td>1</td>
       <td>2</td>
       <td>3</td>
@@ -31,53 +33,55 @@ function drawTable() {
     </table>
 </div>
 <div class="hotkeys-table">
-  <h4 class="text-success">In order to use hotkeys, find your operating system and browser in the table</h4>
+  <h4 class="text-success">${switchLang().secondTableTitle}</h4>
   <table>
     <tr>
-      <th>Browser</th>
+      <th>${switchLang().browser}</th>
       <th>Windows</th>
       <th>Linux</th>
       <th>Mac</th>
     </tr>
     <tr>
       <td class="fw-bold">Chrome</td>
-      <td>[Alt] + hotkey</td>
-      <td>[Alt] + hotkey</td>
-      <td>[Control] + [Alt] + hotkey</td>
+      <td>[Alt] + ${switchLang().hotkey}</td>
+      <td>[Alt] + ${switchLang().hotkey}</td>
+      <td>[Control] + [Alt] + ${switchLang().hotkey}</td>
     </tr>
     <tr>
       <td class="fw-bold">Edge</td>
-      <td>[Alt] + hotkey</td>
+      <td>[Alt] + ${switchLang().hotkey}</td>
       <td>N/A</td>
       <td>N/A</td>
     </tr>
     <tr>
       <td class="fw-bold">Internet Explorer</td>
-      <td>[Alt] + hotkey</td>
+      <td>[Alt] + ${switchLang().hotkey}</td>
       <td>N/A</td>
       <td>N/A</td>
     </tr>
     <tr>
       <td class="fw-bold">Firefox</td>
-      <td>[Alt] + [Shift] + hotkey</td>
-      <td>[Alt] + [Shift] + hotkey</td>
-      <td>[Control] + [Alt] + hotkey</td>
+      <td>[Alt] + [Shift] + ${switchLang().hotkey}</td>
+      <td>[Alt] + [Shift] + ${switchLang().hotkey}</td>
+      <td>[Control] + [Alt] + ${switchLang().hotkey}</td>
     </tr>
     <tr>
       <td class="fw-bold">Safari</td>
-      <td>[Alt] + hotkey</td>
+      <td>[Alt] + ${switchLang().hotkey}</td>
       <td>N/A</td>
-      <td>[Control] + [Alt] + hotkey</td>
+      <td>[Control] + [Alt] + ${switchLang().hotkey}</td>
     </tr>
     <tr>
       <td class="fw-bold">Opera</td>
       <td colspan="3">
-        <p>Opera 15 or newer: [Alt] + hotkey</з>
-        <p>Opera 12.1 or older: [Shift] + [Esc] + hotkey</p>
+        <p>Opera 15 or newer: [Alt] + ${switchLang().hotkey}</з>
+        <p>Opera 12.1 or older: [Shift] + [Esc] + ${switchLang().hotkey}</p>
       </td>
     </tr>
   </table>
-</div>`);
+</div>`;
+
+  mainContent.insertAdjacentHTML('beforeend', html);
 }
 
 export default function drawHotKeys() {
